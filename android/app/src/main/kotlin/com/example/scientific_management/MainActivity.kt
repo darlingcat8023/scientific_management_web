19cb0016c374dbddf25f9299a5e88f752e97e3ee
张朝龙 <zcl0621@hotmail.com>
2022-04-03 21:27:39 +0800

    remove

--- android/app/src/main/kotlin/com/example/scientific_management/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scientific_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/scientific_management_web/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scientific_management_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
